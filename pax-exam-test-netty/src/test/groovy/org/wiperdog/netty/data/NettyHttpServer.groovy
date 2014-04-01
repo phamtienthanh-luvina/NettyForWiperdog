9c06c729d2227b0be6b7f41f01063e3ef9dc6400
nguyenxuanluong-luvina <luongnx512@gmail.com>
2014-04-01 17:36:56 +0700

    Update NettyHttpServer.groovy

--- pax-exam-test-netty/src/test/groovy/org/wiperdog/netty/data/NettyHttpServer.groovy
+++ pax-exam-test-netty/src/test/groovy/org/wiperdog/netty/data/NettyHttpServer.groovy
@@ -4,57 +4,72 @@ import net.javaforge.netty.servlet.bridge.ServletBridgeChannelPipelineFactory;
 import net.javaforge.netty.servlet.bridge.config.ServletConfiguration;
 import net.javaforge.netty.servlet.bridge.config.WebappConfiguration;
 
+import org.codehaus.groovy.control.CompilationFailedException;
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.io.FileType;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class NettyHttpServer {
 
-    private static final Logger log = LoggerFactory.getLogger(NettyHttpServer.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(NettyHttpServer.class);
 
-    public static void startServer() {
-    	int port = 8080;
-        long start = System.currentTimeMillis();
+	public static void startServer() throws CompilationFailedException, IOException {
+		int port = 8080;
+		
+		// Path to folder contains servlet
+        File dirServlet = new File("/org/wiperdog/netty/data/servlet/");
+        
+		long start = System.currentTimeMillis();
 
-        // Configure the server.
-        final ServerBootstrap bootstrap = new ServerBootstrap(
-                new NioServerSocketChannelFactory(Executors
-                        .newCachedThreadPool(), Executors.newCachedThreadPool()));
+		// Configure the server.
+		final ServerBootstrap bootstrap = new ServerBootstrap(
+				new NioServerSocketChannelFactory(
+						Executors.newCachedThreadPool(),
+						Executors.newCachedThreadPool()));
 
-        //Registering servlet in servlet container
- 
-        WebappConfiguration webapp = new WebappConfiguration();
-        webapp.addServletConfigurations(new ServletConfiguration(
-                TestServlet.class, "/servlet/*").addInitParameter("params1",
-                "value1"));
-        webapp.addServletConfigurations(new ServletConfiguration(
-                TestServlet2.class, "/servlet2/*").addInitParameter("params2",
-                "value2"));
-        // Set up the event pipeline factory.
-        final ServletBridgeChannelPipelineFactory servletBridge = new ServletBridgeChannelPipelineFactory(
-                webapp);
-        bootstrap.setPipelineFactory(servletBridge);
+		// Registering servlet in servlet container
+		WebappConfiguration webapp = new WebappConfiguration();
+		if (dirServlet.exists()) {
+			dirServlet.eachFileRecurse(FileType.FILES) { file ->
+				def servletClass = new GroovyClassLoader().parseClass(new File(file.getAbsolutePath()))
+				webapp.addServletConfigurations(new ServletConfiguration(servletClass, "/" + servletClass.getSimpleName() + "/*"));
+			}
+		}
+		
+		// Set up the event pipeline factory.
+		final ServletBridgeChannelPipelineFactory servletBridge = new ServletBridgeChannelPipelineFactory(
+				webapp);
+		bootstrap.setPipelineFactory(servletBridge);
 
-        // Bind and start to accept incoming connections.
-        final Channel serverChannel = bootstrap
-                .bind(new InetSocketAddress(port));
+		// Bind and start to accept incoming connections.
+		final Channel serverChannel = bootstrap
+				.bind(new InetSocketAddress(port));
 
-        long end = System.currentTimeMillis();
-        log.info(">>> Server started in {} ms .... <<< ", (end - start));
+		long end = System.currentTimeMillis();
+		log.info(">>> Server started in {} ms .... <<< ", (end - start));
 
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-
-            @Override
-            public void run() {
-                servletBridge.shutdown();
-                serverChannel.close().awaitUninterruptibly();
-                bootstrap.releaseExternalResources();
-            }
-        });
-
-    }
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				servletBridge.shutdown();
+				serverChannel.close().awaitUninterruptibly();
+				bootstrap.releaseExternalResources();
+			}
+		});
+	}
 }
