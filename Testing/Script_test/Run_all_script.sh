b21ac776edbce688fcc28c0e0004adf0ac15dd35
nguyenxuanluong-luvina <luongnx512@gmail.com>
2014-04-18 14:12:02 +0700

    Update shell script test

---
+++ Testing/Script_test/Run_all_script.sh
@@ -0,0 +1,10 @@
+
+./DBCommonConfig.sh
+./DBConfigServlet.sh
+./DestTest.sh
+./ImportInstanceServlet.sh
+./JobDeclaredServlet.sh
+./JobDocInfoServlet.sh
+./LogFileInfoServlet.sh
+./PolicyServlet.sh
+./TestJobServlet.sh
